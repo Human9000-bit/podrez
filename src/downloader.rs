6aba670ae8ad263fa8e7d6975e12528811e4f631
human9000 <human9000.2021@gmail.com>
2024-07-16 19:22:55 +0500

    trying to implement index.json parsing

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,10 +1,8 @@
-use core::panicking::panic;
-use std::{error::Error, fs::{DirBuilder, ReadDir}, io::{Read, Write}, net::TcpStream, path::{PathBuf, Path}};
-
+use std::{fs::{DirBuilder, ReadDir}, io::{Read, Write}, net::TcpStream, path::{PathBuf, Path}};
 
 pub fn path_handler(path: &PathBuf) -> Option<ReadDir> {
     if !path.exists() {
-        DirBuilder::new()
+        DirBuilder::new() // if folder doesnt exist create .sounds dir and download sounds into it
             .recursive(true)
             .create(path).unwrap();
         
@@ -20,30 +18,43 @@ pub fn path_handler(path: &PathBuf) -> Option<ReadDir> {
     Some(iter)
 }
 
-pub fn download_files(path: &Path) {
-    match get("https://mipoh.ru/podrez/index.json") {
-        Err(e) => panic!("{:?}", e),
+pub fn download_files(path: &Path) { //downloads json file with list of urls
+    let resp = get("");
+    let urls = parse_index(resp);
+    
+    for i in urls { //iterates over array or urls, download file from every url and write it.
+        let resp = get(i);
+        let parts: Vec<&str> = i.split('/').collect();
+        let name = parts.last();
+        drop(parts);
         
-        Ok(index) => {let urls = parse_index(index);}
-    };
+        write_file
+    }
 }
 
-pub fn get(url: &str) -> Result<String, Box<dyn Error>> {
-    let url_parts: Vec<&str> = url.split("/").collect();
+pub fn get(url: &str) -> String { // GET request using std::net
+    let url_parts: Vec<&str> = url.split('/').collect();
     let host = url_parts[2];
     let path = "/".to_string() + &url_parts[3..].join("/");
     
-    let mut stream = TcpStream::connect(host)?;
+    let mut stream = TcpStream::connect(host).unwrap();
     
     let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
-    stream.write_all(request.as_bytes())?;
+    stream.write_all(request.as_bytes());
     
     let mut response = String::new();
-    stream.read_to_string(&mut response)?;
+    stream.read_to_string(&mut response);
     
-    Ok(response)
+    response
 }
 
-pub fn parse_index(index: String) -> Vec<&str> {
+pub fn parse_index(index: String) -> Vec<&'static str> { //parses json file and returns an array of urls
+    let str: &str = &index;
+    let parsed = json::parse(str);
+    
+    parsed.unwrap();
+}
+
+fn write_file(path: PathBuf, filename: &str) {
     
 }
