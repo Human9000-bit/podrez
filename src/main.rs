441d3a1f6e4c387b1fa2b5dd9331a276789e6521
human9000 <human9000.2021@gmail.com>
2024-07-16 19:21:47 +0500

    documented main

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,20 @@ use rusty_audio::prelude::*;
 use dirs::home_dir;
 
 fn main() {
-    thread::sleep(Duration::from_secs(20*60)); //спим 20 минут чтобы она не сразу орала
+    thread::sleep(Duration::from_secs(20*60)); //sleeps for 20 mins
     loop {
         let mut rngl = rand::thread_rng();
-        thread::sleep(Duration::new(rngl.gen_range(15 * 60..35 * 60), 0));
+        thread::sleep(Duration::new(rngl.gen_range(15 * 60..35 * 60), 0)); //sleeps randomly from 15 to 35 mins
         let mut filesarr = Vec::new();
         
-        let path = home_dir().unwrap().join(".sounds");
+        let path = home_dir().unwrap().join(".sounds"); // the path of sounds dir. used dir crate because of diiferenco of home dr in unix and windows
         
         let iter = match path_handler(&path) {
             Some(value) => value,
             None => continue,
         };
         
-        //пробегаем по всем файлам в папке и на рандом выбираем звук
+        //iterating over all files in directory and picking a random sound
         let files = ReadDir::into_iter(iter);
 
         for i in files {
@@ -31,13 +31,12 @@ fn main() {
         }
 
         println!("{:?}", filesarr);
-        // let mut rngl = rand::thread_rng();
-        let num = rngl.gen_range(0..filesarr.len());
+        let num = rngl.gen_range(0..filesarr.len()); //random index
         play(filesarr[num].clone())
     }
 }
 
-fn play(path: String) {
+fn play(path: String) { //plays the very sound
     let mut audio = Audio::new();
     audio.add("sound", path);
     audio.play("sound");
